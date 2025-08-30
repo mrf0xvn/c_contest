d07d2d1991b95486a51d439c3138a854873e28e2
Hau Nguyen <trunghau6404@gmail.com>
2025-08-31 02:05:37 +0700

    done

--- Bai12/main.c
+++ Bai12/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    int t,n;
-    scanf("%d%d", &t, &n);
-    if ()
-    return 0;
+    int t, n;
+    scanf("%d %d", &t, &n);
+    if (t >= 1 && t <= 12 && n > 0) {
+        if (t == 1 || t == 3 || t == 5 || t == 7 || t == 8 || t == 10 || t == 12) {
+            printf("31");
+        } else if (t == 4 || t == 6 || t == 9 || t == 11) {
+            printf("30");
+        }
+        else {
+            if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
+                printf("29");
+            }
+            else {
+                printf("28");
+            }
+        }
+    }
+    else {
+        printf("INVALID");
+    }
 }
